a42e0d83ba49aa87b13a602b5e8b082ed8b8dd11
Jean <jean.mainguy@outlook.fr>
2018-06-25 10:36:50 +0200

    add new graph

--- scripts/R_scripts/homogeneity_evaluation_plot.r
+++ scripts/R_scripts/homogeneity_evaluation_plot.r
@@ -15,7 +15,7 @@ basename = args[2]
 output_dir = basename# paste("results/figures", basename, sep = "/")
 
 data = read.csv(file = csv_file, sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-data = data[data$unclassified_cluster == "False",] 
+data = data[data$unclassified_cluster == "False",]
 # Density plot
 
 p = ggplot(data, aes(x=homogeneity)) + geom_density(aes(group=inflation, colour=inflation), alpha=0) + theme_minimal()
@@ -27,20 +27,20 @@ p
 dev.off()
 
 
-#COLOR BY DOMAIN ANNOTATIONS
-#We don't display the name of first node that appears less than  time
+#COLOR BY FISR NODE
+#We don't display the name of first node that appears less than n time
 #They are labeled as Other
 first_nodes = table(data$first_node)
 first_nodes = data.frame(first_nodes, stringsAsFactors = FALSE)
 colnames(first_nodes) = c('First_node', 'Freq')
-other_nodes = first_nodes$First_node[first_nodes$Freq < 50]
+other_nodes = first_nodes$First_node[first_nodes$Freq < 30]
 data$first_node[data$first_node %in% other_nodes] = "Other"
 
-p = ggplot(data, aes(x=homogeneity)) + geom_density(aes(colour=first_node, group=first_node), alpha=0) + theme_minimal()
+p = ggplot(data, aes(x=homogeneity)) + geom_density(aes(colour=first_node, group=first_node, fill=first_node), alpha=0.4) + theme_minimal()
 
 output_file = paste(output_dir, 'homogeneity_density_first_node.png', sep ='')
 
-png(filename=output_file,  width = 1800, height = 600)
+png(filename=output_file,  width = 1368, height = 768)
 p
 dev.off()
 
@@ -50,6 +50,25 @@ p = ggplot(data, aes(x=homogeneity)) + geom_density(aes(colour=polyprotein, grou
 
 output_file = paste(output_dir, 'homogeneity_density_polyprot.png', sep ='')
 
-png(filename=output_file,  width = 1800, height = 600)
+png(filename=output_file,  width = 1368, height = 768)
+p
+dev.off()
+
+plot(data$number_of_genome, data$homogeneity)
+#Plot le nombre de sequence/genome par cluster
+data = data[data$nb_polyprotein != 0,]
+data_cnt = data.frame( table(data$number_of_genome))
+colnames(data_cnt) = c("number_of_genome", "Number_of_Cluster")
+
+p<-ggplot(data=data_cnt, aes(x=number_of_genome, y=Number_of_Cluster)) +
+  geom_bar(stat="identity") +  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 0, size=15, vjust=0.5, hjust=1),
+        axis.text.y = element_text(size=15), axis.title=element_text(size=15,face="bold"),
+        legend.text = element_text( size=15), legend.title = element_text(size=15, face='bold')) +
+  labs( y="Number of genome per clusters", x="number_of_genome")
+
+
+output_file = paste(output_dir, 'Cluster_with_Polyprotein.png', sep ='')
+png(filename=output_file,  width = 800, height = 500)
 p
 dev.off()
