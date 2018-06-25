f13c6698e49b95cf54788878eaa69fd7c33753bc
Jean <jean.mainguy@outlook.fr>
2018-06-25 10:36:25 +0200

    visualisation of graph with ETT3

--- scripts/homogeneity_test.sh
+++ scripts/homogeneity_test.sh
@@ -8,16 +8,16 @@ unclassified_term_file="data/taxonomy/unclassified_terms.txt"
 # cluster_dir='data/clustering_result/Retro-transcribing_viruses/inflation_test/'
 # homogeneity_dir='data/clustering_result/Retro-transcribing_viruses/homogeneity_evaluation/'
 #
+# 
+# file=data/clustering_result/Viruses/inflation_test/Viruses_1e-5_coverage80_I1_4.out
+# file="data/clustering_result/Alphavirus/inflation_test/Alphavirus_1e-5_coverage50_I1_8.out"
+# file="data/clustering_result/Alphavirus/inflation_test/Alphavirus_1e-5_coverage90_I1_2.out"
+# file='test/inflation_test/Viruses_1e-5_coverage50_I8.out'
 
-file=data/clustering_result/Viruses/inflation_test/Viruses_1e-5_coverage80_I1_4.out
-file="data/clustering_result/Alphavirus/inflation_test/Alphavirus_1e-5_coverage50_I1_8.out"
-file="data/clustering_result/Alphavirus/inflation_test/Alphavirus_1e-5_coverage90_I1_2.out"
-file='data/clustering_result/Viruses/inflation_test/Viruses_1e-5_coverage50_I8.out'
-
-homogeneity_dir='test/'
+file='test/Viruses_1e-5_coverage50_I1_4.out'
 
 #
-python3 scripts/taxonomic_homogeneity_checking.py $file $homogeneity_dir $taxonomy_file $alternative_taxon_id_file $unclassified_term_file visualisation
+python3 scripts/visualisation_taxonomic_tree_cluster.py $file
 
 # prefix_plot_file=$(basename $homogeneity_dir)
 # Rscript scripts/R_scripts/homogeneity_evaluation_plot.r ${homogeneity_dir}Viruses_1e-5_coverage80_I1_4_tax_homogeneity.csv test_plot
