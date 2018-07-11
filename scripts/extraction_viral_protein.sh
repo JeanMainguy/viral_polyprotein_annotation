4ac39a7af114da4ee7aedc9d59b6d2701937426a
Jean <jean.mainguy@outlook.fr>
2018-07-11 16:45:47 +0200

    small change

--- scripts/extraction_viral_protein.sh
+++ scripts/extraction_viral_protein.sh
@@ -9,7 +9,10 @@ stat_output_dir='results/stat_viral_protein/'
 taxon='ssRNA viruses'
 taxon='Alphavirus'
 taxon='Viruses'
-taxon="Retro-transcribing viruses"
+# taxon='1198013'
+# taxon="Retro-transcribing viruses"
+# taxon='dsDNA viruses, no RNA stage'
+# taxon='Caudovirales'
 tresholdSP="90" # length threshold in aa for first peptide annotation corresponding to SP
 rm -rf /tmp/$USER/
 tmpdir=/tmp/$USER/
@@ -18,7 +21,7 @@ mkdir -p ${tmpdir}$stat_output_dir
 
 echo extraction of viral proteins...
 
-/usr/bin/time python3 scripts/viral_protein_extraction.py "${taxon}" ${tmpdir}$seq_output_dir $taxonomy_file  ${tresholdSP} ${tmpdir}$stat_output_dir
+python3 scripts/viral_protein_extraction.py "${taxon}" ${tmpdir}$seq_output_dir $taxonomy_file  ${tresholdSP} # ${tmpdir}$stat_output_dir
 
 echo mv file from tmp to final dir
 mv ${tmpdir}${seq_output_dir}*  ${seq_output_dir}
@@ -27,3 +30,6 @@ mv ${tmpdir}${stat_output_dir}*  $stat_output_dir
 rm -rf /tmp/$USER/
 
 echo END
+
+#To extract line of the stat protein with only protein that have peptide annotation
+## cat stat_proteins_Viruses.csv | cut -f2-18 | awk '$4 != "0"' |  awk '$8 != "True"' > stat_potential_polyproteins_Viruses.csv
