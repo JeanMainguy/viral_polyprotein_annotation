50e1ff579b42c99301be8cb955cb8e1a0dd19edb
Jean <jean.mainguy@outlook.fr>
2018-07-11 16:38:06 +0200

    improvement of the sbatch export var etc

--- scripts/multiple_alignment_proteins.sh
+++ scripts/multiple_alignment_proteins.sh
@@ -1,79 +1,78 @@
+#!/bin/bash
+#
+#SBATCH --job-name=multiple_alignement
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=ALL
+#SBATCH --mem=4GB
+#SBATCH --output=log/%x-%j.out
+#SBATCH --error=log/%x-%j.out
+
+# set -e # exit if command fail
+
 module load clustalomega
-module load pyfasta
 
+#Receive cluster_file
+#Alignment DIR
+# and faa_db
+echo multiplealignment dir $alignement_dir
+echo cluster_file $cluster_file
+echo faa db $faa_db
+force=true
+if [ -z "$alignement_dir" ];
+then
+  alignement_dir=data/alignment/Viruses_evalue_1e-20coverage50_I1_8/
+  mkdir -p $alignement_dir
 
+fi
 
-taxon='Alphavirus'
-faa_db="data/viral_proteins/Alphavirus_protein_db.faa"
-cluster_file="data/clustering_result/Alphavirus/inflation_test/Alphavirus_1e-5_coverage60_I1_6.out"
-# taxon="Retro-transcribing viruses"
-# cluster_file='data/clustering_result/Retro-transcribing_viruses/inflation_test/Retro-transcribing_viruses_1e-5_coverage90_I3.out'
-# faa_db="data/viral_proteins/Retro-transcribing_viruses_protein_db.faa"
-
-# taxon='Viruses'
-# cluster_file='data/clustering_result/Viruses/inflation_test/Viruses_1e-5_coverage90_I2.out'
-# faa_db="data/viral_proteins/Viruses_protein_db.faa"
-
+if [ -z "$cluster_file" ];
+then
+  cluster_file='data/clustering_result/Viruses/clustering_parameter_variation/Viruses_evalue_1e-20coverage50_I1_8.out'
+fi
+if [ -z "$faa_db" ];
+then
+  faa_db="data/viral_proteins/Viruses_protein_db.faa"
+fi
 
 if [ -z "$TMPDIR" ];
 then
-  TMPDIR=/tmp/$USER/
+  TMPDIR=/tmp/$USER
+  mkdir -p ${TMPDIR}
 fi
 
-taxon=${taxon// /_} # replace space by underscore
-taxon=${taxon//,/} # replace coma by nothing
+echo multiplealignment dir $alignement_dir
+echo cluster_file $cluster_file
+echo faa db $faa_db
 
-evalue="1e-5"
-
-seq_faa_dir=${TMPDIR}cluster_faa_sequences/
-# seq_faa_dir=data/alignment/cluster_faa_sequences/
-mkdir -p $seq_faa_dir
-
-echo cluster protein extraction and multiplealignment
-
-name_dir=$(basename $cluster_file)
-name_dir=${name_dir%.*}
-alignement_dir=data/alignment/$name_dir/
 
 mkdir -p $alignement_dir
-mkdir -p ${TMPDIR}$alignement_dir
-
-interpro_result_dir=data/interpro_results/$name_dir/
-mkdir -p $interpro_result_dir
-mkdir -p ${TMPDIR}$interpro_result_dir
-
 
 echo TMPDIR ${TMPDIR}
 
 var=0
 while read l;
 do
+  cluster_faa_base=seq_cluster${var}
 
-  if [[ $l = *"Peptide"* ]]; then
+  if [ ! -f ${alignement_dir}${cluster_faa_base}.aln ] || [ "$force" == true ]; then
 
-    echo $l | sed -e 'y/ /\n/' > ${seq_faa_dir}ids.txt #replace tab by newline
-    cluster_faa_base=seq_cluster${var}
+    echo $l | sed -e 'y/ /\n/' > ${TMPDIR}/ids.txt #replace tab by newline
     # command from http://bioinformatics.cvr.ac.uk/blog/short-command-lines-for-manipulation-fastq-and-fasta-sequence-files/
     #extract faa seq in a new file
-    perl -ne 'if(/^>(\S+)/){$c=$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' ${seq_faa_dir}ids.txt $faa_db > ${seq_faa_dir}$cluster_faa_base.faa
+    perl -ne 'if(/^>(\S+)/){$c=$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' ${TMPDIR}/ids.txt $faa_db > ${TMPDIR}/$cluster_faa_base.faa
 
-    output=${TMPDIR}${alignement_dir}${cluster_faa_base}.aln
-    interpro_output=${TMPDIR}${interpro_result_dir}${cluster_faa_base}
+    output=${TMPDIR}/${cluster_faa_base}.aln
     echo ALIGNEMENT of $output
+    /usr/bin/time clustalo -i ${TMPDIR}/$cluster_faa_base.faa -o $output --outfmt=clu --threads=8
 
-    /usr/bin/time clustalo -i ${seq_faa_dir}$cluster_faa_base.faa -o $output --outfmt=clu  --wrap=150
-    /usr/bin/time /localmirror/monthly/interpro/interproscan-5.29-68.0/interproscan.sh --appl PFAM,CDD,ProDom,SMART,ProSiteProfiles -i ${seq_faa_dir}$cluster_faa_base.faa -b ${interpro_output} -f GFF3
-
+  else
+    echo the file ${alignement_dir}${cluster_faa_base}.aln  exist already. We dont recompute the clustering
   fi
+  echo $var
   ((var++)) #increment var to know which cluster we are
+  echo $var
+
 done <$cluster_file
 
-mv ${TMPDIR}${alignement_dir}* ${alignement_dir}
-mv ${TMPDIR}${interpro_result_dir}* ${interpro_result_dir}
+mv ${TMPDIR}/*.aln ${alignement_dir}
 rm -rf ${TMPDIR}
-
-# for aln in $alignement_dir*aln;
-# do
-#   name=${aln%.*}.faa
-#   /apps/emboss/6.6.0/bin/seqret -sequence $aln -sformat1 clustal -osformat2 fasta  -outseq $name
-# done
