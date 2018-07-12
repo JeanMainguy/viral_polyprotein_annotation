702ac1ccefdaf38e95133c84aee7db6a7924bb30
Jean <jean.mainguy@outlook.fr>
2018-07-12 20:20:03 +0200

    parser works fine with new implementation

--- scripts/parser_interpro_results.py
+++ scripts/parser_interpro_results.py
@@ -1,5 +1,8 @@
+import re
+import viral_genome_classes as obj
 
-def getMatchObject(self, gff_file):
+
+def getMatchObject(genome, gff_file):
     #retreive all the match that belong to the genome
     flag = False
     with open(gff_file) as fl:
@@ -8,13 +11,14 @@ def getMatchObject(self, gff_file):
             if l.startswith(">") or l.startswith("##FASTA"):
                 break
             if l.startswith('##sequence-region'):
-                flag = True if l.startswith('##sequence-region {}|{}'.format(self.expectation_node,self.taxon_id)) else False
+                flag = True if l.startswith('##sequence-region {}|'.format(genome.taxon_id)) else False
                 continue
             if flag:
+                # print(l)
                 (taxseqid, method, feature_type, start, end, score, strand, phase, attributes) = l.split("\t")
                 #match = {"start":int(begin), "end":int(end), "app":method, "seqid":seqid, "taxid":taxid}
                 if feature_type == 'protein_match':
-                    (expectation_node, taxid, seqid, polyprotein_number) = taxseqid.split("|")
+                    (taxid, seqid, length) = taxseqid.split("|")
                     re_result = re.search("ID=match\$([\d]+)_[\d]+_[\d]+", attributes)
                     matchID = "" if not re_result else int(re_result.group(1))
 
@@ -30,12 +34,25 @@ def getMatchObject(self, gff_file):
                     re_result = re.search('Dbxref="([^"]+)"', attributes)
                     Dbxref= "" if not re_result else re_result.group(1)
 
-                    self.matchs.append(Match(taxid, seqid,  method, start, end, score, Dbxref, Name, matchID, signature_desc))
+                    genome.matchs.append(obj.Match(taxid, seqid,  method, start, end, score, Dbxref, Name, matchID, signature_desc))
+
+def associateMatchWithPolyprotein(genome):
+    for segment in genome.segments:
+        for cds in segment.cds:
+            # cds.matchs = [match for match in self.matchs if match.seqid == cds.protein_id]
+            for match in genome.matchs:
+                if match.seqid == cds.protein_id:
+                    cds.matchs.append(match)
+                    match.protein = cds
+                    match.getGenomicPositions(cds.start)
+                    segment.matchs.add(match)
+        getDomainOverlappingInfo(segment)
+        identifyDuplicatedMatch(segment)
 
 
-def getDomainOverlappingInfo(self):
+def getDomainOverlappingInfo(segment):
 
-    for poly in self.polyproteins:
+    for poly in segment.polyproteins:
         for pep in list(poly.peptides) + poly.unannotated_region:
 
             if getattr(pep, 'parent_peptide', False):
@@ -87,20 +104,20 @@ def getDomainOverlappingInfo(self):
                 m.left_overlaps_peptide = pep_start - m.start_in_prot
 
 
-def identifyDuplicatedMatch(self):
+def identifyDuplicatedMatch(segment):
     # Due to ribosomal_slippage 2 proteins have been given to interpro but they share a similar part and then may share same somain
     # To not count twice this domain we identify the domain that have the same start and end in the genome and have same name
 
     # The structure is not that efficient because the duplicated match check the other match and it is not usefull they should be remove
-    matchs = list(self.matchs)
+    matchs = list(segment.matchs)
     for i, match in enumerate(matchs):
         for match_next in matchs[i+1:]:
             if match.isEqualTo(match_next):
                 match_next.duplicated = True
 
 
-def getStringIndices(self, conversion):
-    # print(conversion,' start', self.start/conversion, 'end', (self.end-1)/conversion)
-    # print(conversion,' start', int(self.start/conversion), 'end', int((self.end-1)/conversion))
-    return (int(self.start/conversion), int((self.end-1)/conversion))
-    # return round(self.bp_obj.location.start/conversion), round(self.bp_obj.location.end/conversion)
+def getStringIndices(segment, conversion):
+    # print(conversion,' start', segment.start/conversion, 'end', (segment.end-1)/conversion)
+    # print(conversion,' start', int(segment.start/conversion), 'end', int((segment.end-1)/conversion))
+    return (int(segment.start/conversion), int((segment.end-1)/conversion))
+    # return round(segment.bp_obj.location.start/conversion), round(segment.bp_obj.location.end/conversion)
