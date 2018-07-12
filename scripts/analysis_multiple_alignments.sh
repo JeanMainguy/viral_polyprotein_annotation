cef893d43049f68f2b898a1026bb73669cad213f
Jean <jean.mainguy@outlook.fr>
2018-07-12 20:21:37 +0200

    bash script for multiple aln

--- scripts/analysis_multiple_alignments.sh
+++ scripts/analysis_multiple_alignments.sh
@@ -2,7 +2,11 @@
 
 
 taxonomy_file="data/taxonomy/taxonomy_virus.txt"
-alignment_dir="data/alignment/Viruses_1e-5_coverage90_I2/"
+# alignment_dir="data/alignment/Viruses_1e-5_coverage90_I2/"
+alignment_dir='data/alignment/Viruses_evalue_1e-40coverage40_I2/'
+
+
+
 window=20
 
 rm ${alignment_dir}stat_of_all_cluster.csv
@@ -12,19 +16,23 @@ do
 
   output_file=${aln%.*}.csv
   echo $output_file
-  python3 scripts/multiple_alignment_analysis.py $aln $output_file $taxonomy_file $window
+  # python3 scripts/multiple_alignment_analysis.py $aln $output_file $taxonomy_file $window
   cat $output_file >> ${alignment_dir}stat_of_all_cluster.csv
 done
 
 mkdir -p /tmp/$USER/
-grep ^'cluster' ${alignment_dir}stat_of_all_cluster.csv | uniq > /tmp/$USER/tmp_file
-grep -v ^'cluster' ${alignment_dir}stat_of_all_cluster.csv >> /tmp/$USER/tmp_file
-cat /tmp/$USER/tmp_file > ${alignment_dir}stat_of_all_cluster.csv
+grep ^'cluster' ${alignment_dir}stat_of_all_cluster.csv | uniq > tmp
+grep -v ^'cluster' ${alignment_dir}stat_of_all_cluster.csv >> tmp
+cat tmp > ${alignment_dir}stat_of_all_cluster.csv
 
 rm -rf /tmp/$USER/
 
 
-
-#Make graph from the csv file result
-prefix_plot_file=$(basename $alignment_dir)
-Rscript scripts/R_scripts/alignment_analysis_plot.r ${alignment_dir}stat_of_all_cluster.csv $prefix_plot_file
+#
+# #Make graph from the csv file result
+# prefix_plot_file=$(basename $alignment_dir)
+# graph_output_dir="results/figures/alignment_analysis_plot/$prefix_plot_file/"
+#
+# mkdir -p $graph_output_dir
+#
+# Rscript scripts/R_scripts/alignment_analysis_plot.r ${alignment_dir}stat_of_all_cluster.csv $graph_output_dir
