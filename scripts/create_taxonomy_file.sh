db1cb5c2932d870db830249be1030d1adff754dd
Jean <jean.mainguy@outlook.fr>
2018-07-18 16:31:31 +0200

    add mkdir for tmp dir and set -e

--- scripts/create_taxonomy_file.sh
+++ scripts/create_taxonomy_file.sh
@@ -33,8 +33,10 @@ echo $outputTime
 echo $ncbi_refseq_dbTime
 
 
+set -e # exit if command fail.. this command here because before the stat commands can fail...
 
 TMPDIR=/tmp/$USER/
+mkdir -p $TMPDIR
 
 
 if [ ! -f ${output_dir}$output_file ] || [ $ncbi_refseq_dbTime -gt $outputTime ] || [ "$force" == true ];
@@ -44,7 +46,7 @@ then
   tar -Oxf ${ncbi_current}taxonomy/new_taxdump/new_taxdump.tar.gz nodes.dmp | cut -d$'\t' -f1,13 > ${TMPDIR}$taxon_id_gencode_file # extract taxon id and the corresponding genetic code
 
   echo "creation of taxonomy file with for each taxon id the path to the genbank file in refseq db"
-  python3 scripts/taxonomy.py ${TMPDIR}${output_file} ${ncbi_refseq_db} ${TMPDIR}${taxon_id_gencode_file} ${TMPDIR}$alternative_taxon_id #2> ${output_dir}taxonomy_file_creation.log
+  python3 scripts/taxonomy.py ${TMPDIR}${output_file} ${ncbi_refseq_db} ${TMPDIR}${taxon_id_gencode_file} ${TMPDIR}$alternative_taxon_id 2> ${output_dir}taxonomy_file_creation.log
   echo mv taxonomy files in final dir $output_dir
   mv ${TMPDIR}${output_file} $output_dir
   mv ${TMPDIR}${taxon_id_gencode_file} $output_dir
@@ -61,3 +63,6 @@ find data/taxonomy/  -type l -delete
 echo create taxonomy symb link files
 real_path_outputdir=`realpath ${output_dir}`
 ln -s $real_path_outputdir/*.txt data/taxonomy/
+
+
+echo END of create taxonomy script 
