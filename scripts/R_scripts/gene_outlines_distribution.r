5d69e314a6c97139b004563d017c80595314936d
Jean <jean.mainguy@outlook.fr>
2018-07-20 20:07:28 +0200

    csv file of stat

---
+++ scripts/R_scripts/gene_outlines_distribution.r
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+library(ggplot2)
+
+csv_file="results/stat_viral_protein/stat_proteins_Viruses.csv"
+output_dir = "results/figures/"
+
+data_original = read.csv(file = csv_file, sep = '\t', header = TRUE, stringsAsFactors = FALSE)
+
+data = data_original[data_original$has_peptide == "True" ,]
+
+data$status = ifelse(data$polyprotein_outline == "True", 'Identify polyprotein', data$non_polyprotein_explanation)
+
+data$status = ifelse(data$non_polyprotein_explanation == "Intein outline: extein includes intein", 
+                     'Intein-extein\nannotation', 
+                     data$status)
+data$status = ifelse(data$non_polyprotein_explanation == "Intein outline: extein surounds intein", 
+                     'Intein-extein\nannotation', 
+                     data$status)
+
+data$status = ifelse(data$non_polyprotein_explanation == "single peptide annotaion covering the whole CDS", 
+                     'single peptide annotaion covering almost the whole CDS', 
+                     data$status)
+
+data$status = ifelse(data$status == "single peptide annotaion covering almost the whole CDS", 
+                     'Single peptide\nannotation covering\nalmost the whole CDS', 
+                     data$status)
+
+#SIMPLE HISTOGRAMME WITH outline type
+table = data.frame( table(data$status))
+colnames(table) = c("outline", "Nb_protein")
+
+p <- ggplot(data=table, aes(x=reorder(outline, Nb_protein), y=Nb_protein, fill=outline)) +geom_bar(stat="identity") + theme_minimal() +
+  labs(x =NULL, y="Number of CDS", fill=NULL) + 
+  theme(axis.text=element_text(size=20), axis.title=element_text(size=18,face="bold"))  + guides(fill=FALSE)
+
+#p = p+scale_fill_manual(values=c("#E69F00", "#56B4E9"), guide=FALSE)
+p = p+  geom_text(aes(label=Nb_protein), vjust=-0.1, color="black",
+                  position = position_dodge(0.95), size=7)
+p
+
+name="annotated_protein_outlines_distribution"
+output_file = paste(output_dir, name,".png", sep ='')
+png(filename=output_file,  width = 1000, height = 600)
+p
+dev.off()
+
---
+++ scripts/R_scripts/signal_peptide_plots.r
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+library(ggplot2)
+
+csv_file="results/stat_viral_protein/stat_proteins_Viruses.csv"
+output_dir = "results/figures/"
+sp_length_threshold = 90
+signal_threshold_txt = paste("Signal peptide\nlength threshold =", sp_length_threshold)
+len_txt = nchar(signal_threshold_txt)
+
+data = read.csv(file = csv_file, sep = '\t', header = TRUE, stringsAsFactors = FALSE)
+data = data[data$potential_signal_P_first_pep_len != 'None', ]
+data$potential_signal_P_first_pep_len = sapply(data$potential_signal_P_first_pep_len, as.numeric) 
+
+data$potential_signal_P_first_pep_len_aa = data$potential_signal_P_first_pep_len / 3 
+
+p = ggplot(data, aes(x=potential_signal_P_first_pep_len_aa)) +
+  geom_histogram(aes(y=..density.., fill=potential_signal_P_first_pep_type, colour=potential_signal_P_first_pep_type),      # Histogram with density instead of count on y-axis
+                 binwidth=10) +
+  
+  geom_vline(aes(xintercept=sp_length_threshold),   # Ignore NA values for mean
+             color="black", size=.4,  linetype="dashed")+ 
+  annotate("text", x=sp_length_threshold+280, y = 0.02, label = signal_threshold_txt, size = 5) +   theme_minimal()+
+  theme(axis.text.x = element_text(size=15),
+        axis.text.y = element_text(size=20), axis.title=element_text(size=22,face="bold"),
+        legend.text = element_text( size=20), legend.title =element_text(size=22,face="bold"))+
+        labs( x="length first peptide annotation (aa)", fill="First peptide\nannotation type", colour="First peptide\nannotation type") +
+        scale_x_continuous(breaks=c(sp_length_threshold, seq(200,max(data$potential_signal_P_first_pep_len_aa)+1, 100)))
+  p2 = p + geom_density(alpha=.2, fill="#FF6666")  # Overlay with transparent density plot
+  p
+  p2
+  
+  name="first_peptide_distribution"
+  output_file = paste(output_dir, name,".png", sep ='')
+  png(filename=output_file,  width = 1400, height = 600)
+  p
+  dev.off()
+  
