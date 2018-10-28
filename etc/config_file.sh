462b75f298ab438288e84b69a9c7034891a865ca
Jean <jean.mainguy@outlook.fr>
2018-10-28 13:54:58 +0100

    organisation and cleaning of the config file

--- etc/config_file.sh
+++ etc/config_file.sh
@@ -1,12 +1,7 @@
 ################################################################################
-## INPUT GENOMES
+## INPUT GENOMES DATA BASE
 ################################################################################
-genetic_code_path="genome_db_test/taxonomy/new_taxdump/"
 
-genbank_files_db_path="genome_db_test/genomes/refseq/viral/"
-RefSeq_structure="True"
-# genbank_files_db_path='flat_db_Alphavirus/'
-# RefSeq_structure="False"
 #Structure of the genbank files database : True or False
 # False is a list of genbank files in a folder
 # True same structure as in RefSeq:
@@ -18,43 +13,46 @@ RefSeq_structure="True"
 #             │       └── GCF_000885715.1_ViralProj39601
 #             │           └── GCF_000885715.1_ViralProj39601_genomic.gbff.gz
 
+genetic_code_path="genome_db_test/taxonomy/new_taxdump/"
 
-interpro_path="/mirror/interpro"
+genbank_files_db_path="genome_db_test/genomes/refseq/viral/"
 
-#Create a black list of poorly annotated cds
-black_list_conflict_domain='true'
-black_list_irrelevant_pattern='true'
-confidence_score_treshold=4
-
-
+RefSeq_structure="True"
 
 ################################################################################
 ## VARIABLES
 ################################################################################
 
-## extraction and basic stat
-tresholdSP="90"
+# Taxon : analysis will be performed on every genomes of the database belonging to this taxon
 taxon='Flaviviridae'
-taxon='Alphavirus'
-# taxon='Picornavirales'
 
-# taxon='ssRNA viruses'
-# taxon='Retro-transcribing viruses'
-# taxon='Picornavirales'
+## Signal Peptide length threshold in aa
+tresholdSP="90"
 
 ## blast evalue start
 blast_evalue='1e-5'
 
 ## filtering and mcl clustering
-coverages='60'
-evalues_filtering='1e-60' # 1e-50 1e-20' #'1e-140 1e-160'
+# Can be a list of value separated by a space
+coverages='60' # minimal coverage percentage of the longest sequence between a pair of sequences
+evalues_filtering='1e-60' #
 inflations='1.8'
+
+# Split cluster that have framshiffted proteins
 split_cluster="false"
 
 # multiple alignment anlysis
-WINDOW="25" # used to group cleavage sites
+window="25" # used to group cleavage sites
+confidence_score_treshold=4 # confidence score threshold for the group of cleavage sites
 
-#Conflict between domain annotation and cleavage site annotation
+## IRRELEVANT ANNOTATION IDENTIFICATION:
+# Conflict between domain annotation and cleavage site annotation
+interpro_path="~/path/to_interproscan/folder/"
+
 threshold_overlap_prct=10
 threshold_overlap_aa=15
 ignoring_threshold_ratio=0.5
+
+#Create a black list of poorly annotated cds
+black_list_conflict_domain='true'
+black_list_irrelevant_pattern='true'
